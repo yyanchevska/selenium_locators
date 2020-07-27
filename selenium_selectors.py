a763861ebd8e8210eb2b211019e05c09051b9222
rhonyshniuk <roman.honyshniuk@datarobot.com>
2020-07-27 09:55:09 +0300

    selenium_selectors.py

---
+++ selenium_selectors.py
@@ -0,0 +1,9 @@
+"""
+Add couple selenium tests
+1. Submit filled test form 
+https://demoqa.com/text-box
+2. Click on [Code in it] button after selecting new Category 
+https://testpages.herokuapp.com/styled/basic-ajax-test.html
+3. Print all text in Lorem/Ipsum/Dolor columns 
+https://the-internet.herokuapp.com/challenging_dom#delete
+"""
